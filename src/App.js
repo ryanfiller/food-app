8201f621a65ef9151d9f7e1f60b947eb6fc372aa
Ryan <ryanfiller89@gmail.com>
2017-11-12 20:31:46 -0600

    renames some things, adds jsx to render

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="app horizontal-layout rounded">
+              <nav className="buttons buttons--vertical">
+                  <button className="button button--touch" type="button" name="button">
+                      <span>Quam Pellentesque</span>
+                  </button>
+                  <button className="button button--touch" type="button" name="button">
+                      <span>Vulputate Euismod</span>
+                </button>
+                  <button className="button button--touch" type="button" name="button">
+                      <span>Vestibulum Fusce</span>
+                </button>
+                  <button className="button button--touch" type="button" name="button">
+                      <span>Risus Ullamcorper</span>
+                </button>
+              </nav>
+
+              <main className="main">
+                  <div className="content">
+                      Donec ullamcorper nulla non metus auctor fringilla. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.
+                  </div>
+              </main>
+
+              <nav className="buttons buttons--vertical button--secondary">
+                  <button className="button button--touch" type="button" name="button">
+                      <span>Vestibulum Fusce</span>
+                </button>
+                  <button className="button button--touch" type="button" name="button">
+                      <span>Risus Ullamcorper</span>
+                </button>
+              </nav>
+          </div>
     );
   }
 }
