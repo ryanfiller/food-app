ccd6b377afc3270e1e61ae15244b4f39ae34aae7
Ryan <ryanfiller89@gmail.com>
2017-11-12 21:33:50 -0600

    abstracts nav, adds static filters

--- src/App.js
+++ src/App.js
@@ -2,39 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './app.css';
 
+import NavButtons from './components/NavButtons';
+
 class App extends Component {
   render() {
     return (
         <div className="app horizontal-layout rounded">
-              <nav className="buttons buttons--vertical">
-                  <button className="button button--touch" type="button" name="button">
-                      <span>Quam Pellentesque</span>
-                  </button>
-                  <button className="button button--touch" type="button" name="button">
-                      <span>Vulputate Euismod</span>
+            <NavButtons/>
+
+            <div className="buttons buttons--vertical">
+                <button className="button button--touch" type="button" name="button">
+                    <span>Dairy</span>
                 </button>
-                  <button className="button button--touch" type="button" name="button">
-                      <span>Vestibulum Fusce</span>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Protein</span>
                 </button>
-                  <button className="button button--touch" type="button" name="button">
-                      <span>Risus Ullamcorper</span>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Fruit</span>
                 </button>
-              </nav>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Vegetables</span>
+                </button>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Grains</span>
+                </button>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Spices</span>
+                </button>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Other</span>
+                </button>
+                <button className="button button--touch" type="button" name="button">
+                    <span>Search</span>
+                </button>
+            </div>
 
               <main className="main">
                   <div className="content">
                       Donec ullamcorper nulla non metus auctor fringilla. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.
                   </div>
               </main>
-
-              <nav className="buttons buttons--vertical button--secondary">
-                  <button className="button button--touch" type="button" name="button">
-                      <span>Vestibulum Fusce</span>
-                </button>
-                  <button className="button button--touch" type="button" name="button">
-                      <span>Risus Ullamcorper</span>
-                </button>
-              </nav>
           </div>
     );
   }
---
+++ src/components/NavButtons.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+class NavButtons extends React.Component {
+
+    render() {
+
+        return <nav className="buttons buttons--vertical">
+            <button className="button button--touch" type="button" name="button">
+                <span>Ingredients</span>
+            </button>
+            <button className="button button--touch" type="button" name="button">
+                <span>Recipies</span>
+          </button>
+            <button className="button button--touch" type="button" name="button">
+                <span>Timers</span>
+          </button>
+        </nav>
+    }
+
+}
+
+export default NavButtons;
